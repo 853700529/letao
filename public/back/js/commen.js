15441040514ea70a0e664d337202b47b49121a59
zys520xzz <1639444826@qq.com>
2018-12-27 17:59:32 +0800

    退出登录完成

--- public/back/js/commen.js
+++ public/back/js/commen.js
@@ -22,8 +22,24 @@
     })
     // 退出模态框
     $(".icon_logout").on("click",function(){
-        console.log("jjj");
+        // console.log("jjj");
         $(".modal.fade").modal("show");
-    })
+    });
+    // 确定退出事件
+    $(".logout").on("click",function(){
+        // 发送/ajax告诉服务器需要退出，让服务器把对应d的session销毁
+        $.ajax({
+            type:"GET",
+            url:"/employee/employeeLogout",
+            success:function(info){
+                if(info.success){
+                    location.href="login.html";
+                }
+                if(info.error==400){
+                    alert(info.message);
+                }
+            }
+        })
+    });
 
 });
